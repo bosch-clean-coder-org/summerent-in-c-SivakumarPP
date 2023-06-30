85b513db0f62136a57c933040bc44446313c9304
PES3COB <sivakumar.periannanperumal@bosch.com>
2023-06-30 16:36:34 +0530

    replace switch with if-else

--- typewise-alert.c
+++ typewise-alert.c
@@ -16,17 +16,19 @@ BreachType classifyTemperatureBreach(
   int lowerLimit = 0;
   int upperLimit = 0;
 
-  switch(coolingType) {
-    case PASSIVE_COOLING:
+  if(coolingType == PASSIVE_COOLING)
+  {
       upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      upperLimit = 40;
-      break;
   }
+  else if(coolingType == HI_ACTIVE_COOLING)
+  {
+      upperLimit = 45;
+  }
+  else if(coolingType == MED_ACTIVE_COOLING)
+  {
+      upperLimit = 40;
+  }
+
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
@@ -37,13 +39,13 @@ void checkAndAlert(
     batteryChar.coolingType, temperatureInC
   );
 
-  switch(alertTarget) {
-    case TO_CONTROLLER:
+  if(alertTarget == TO_CONTROLLER)
+  {
       sendToController(breachType);
-      break;
-    case TO_EMAIL:
+  }
+  else if (alertTarget == TO_EMAIL)
+  {
       sendToEmail(breachType);
-      break;
   }
 }
 
@@ -54,16 +56,16 @@ void sendToController(BreachType breachType) {
 
 void sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
+
+  if(breachType == TOO_LOW)
+  {
       printf("To: %s\n", recepient);
       printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
+  }
+  else if (breachType == TOO_HIGH)
+  {
       printf("To: %s\n", recepient);
       printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
   }
+
 }
